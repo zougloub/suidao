55bc1012df87f6235b6d75e441ad2f576e518d40
Jérôme Carretero <cJ@zougloub.eu>
2016-10-07 15:41:26 -0400

    Initial Commit with barely working hts/htc

---
+++ htc.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 vi:noet
+
+__license__ = "GPL v3 - https://www.gnu.org/licenses/gpl.html"
+
+import sys, io, os, fcntl, socket, time, argparse
+import requests
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser(
+	 description="HTTP tunnel",
+	)
+
+	parser.add_argument("url",
+	 help="location where hts.py will answer",
+	)
+
+	try:
+		import argcomplete
+		argcomplete.autocomplete(parser)
+	except:
+		pass
+
+	args = parser.parse_args()
+
+	url = args.url
+
+	fd_i = sys.stdin.fileno()
+	def r():
+		try:
+			return os.read(fd_i, 1024)
+		except OSError:
+			return b""
+
+	flag = fcntl.fcntl(fd_i, fcntl.F_GETFL)
+	fcntl.fcntl(fd_i, fcntl.F_SETFL, flag | os.O_NONBLOCK)
+	flag = fcntl.fcntl(fd_i, fcntl.F_GETFL)
+	if flag & os.O_NONBLOCK == 0:
+		raise Exception("pouet")
+
+	s = requests.Session()
+
+	res = s.put(url, data={})
+	if res.status_code != 201:
+		raise Exception(res)
+
+	t = time.time()
+	while True:
+
+		now = time.time()
+		if now > t + 1e-3:
+			data = r()
+			if data != b"":
+				res = s.post(url, files={"file": data})
+				if res.status_code != 201:
+					raise Exception(res)
+		t = now
+
+		res = s.get(url)
+		if res.status_code != 201:
+			raise Exception(res)
+		sys.stdout.write(res.content)
+		sys.stdout.flush()
+
---
+++ hts.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 vi:noet
+
+__license__ = "GNU AFFERO GENERAL PUBLIC LICENSE v3 - https://www.gnu.org/licenses/agpl-3.0.en.html"
+
+
+import sys, io, os, time, fcntl, socket
+import flask
+
+app = flask.Flask(__name__)
+
+app.config["JSON_AS_ASCII"] = False
+
+sessions = dict()
+
+@app.route("/<path:url>", methods=("PUT",))
+def put(url):
+	if 0:
+		fd_o = sys.stdout.fileno()
+		fd_i = sys.stdin.fileno()
+		def r():
+			try:
+				return os.read(fd_i, 1024)
+			except OSError:
+				return b""
+
+		w = lambda x: os.write(fd_o, x)
+	else:
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		s.connect(("127.0.0.1", 22))
+		fd_i = fd_o = s.fileno()
+		def r():
+			try:
+				return s.recv(1024)
+			except IOError:#socket.error as e:
+				return b""
+		def w(data):
+			while data:
+				n = s.send(data)
+				data = data[n:]
+
+	flag = fcntl.fcntl(fd_i, fcntl.F_GETFL)
+	fcntl.fcntl(fd_i, fcntl.F_SETFL, flag | os.O_NONBLOCK)
+	flag = fcntl.fcntl(fd_i, fcntl.F_GETFL)
+	if flag & os.O_NONBLOCK == 0:
+		raise Exception("pouet")
+
+	sessions[url] = (fd_i, fd_o, r, w)
+
+	return flask.jsonify(
+	 response='success',
+	), 201
+
+
+@app.route("/<path:url>", methods=("GET",))
+def get(url):
+	if url not in sessions:
+		return b"", 404
+	fd_i, fd_o, r, w = sessions[url]
+	#print("get")
+	res = r()
+	return res, 201
+
+@app.route("/<path:url>", methods=("POST",))
+def post(url):
+	if url not in sessions:
+		return b"", 404
+	fd_i, fd_o, r, w = sessions[url]
+	#print("put")
+	b = io.BytesIO()
+	flask.request.files["file"].save(b)
+	w(b.getvalue())
+	#os.fflush(fd_o)
+	#os.flush(fd_o)
+	return flask.jsonify(
+	 response='success',
+	), 201
+
+
+if __name__ == '__main__':
+	app.run(
+	 debug=True,
+	)
+
